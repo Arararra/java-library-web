58ac91cb3872e3cf1189168376be9f699ae635b6
Arararra <55007364+Arararra@users.noreply.github.com>
2025-05-25 23:10:54 +0700

    tidying auth filter

--- src/main/java/com/tugas/perpus/filter/AuthFilter.java
+++ src/main/java/com/tugas/perpus/filter/AuthFilter.java
@@ -9,33 +9,32 @@ import jakarta.servlet.http.HttpSession;
 
 @WebFilter("/*")
 public class AuthFilter implements Filter {
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-        HttpSession session = req.getSession(false);
-        String loginURI = req.getContextPath() + "/login";
-        String uri = req.getRequestURI();
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest req = (HttpServletRequest) request;
+    HttpServletResponse res = (HttpServletResponse) response;
+    HttpSession session = req.getSession(false);
+    String loginURI = req.getContextPath() + "/login";
+    String uri = req.getRequestURI();
 
-        boolean loggedIn = (session != null && session.getAttribute("user") != null);
-        boolean loginRequest = uri.equals(loginURI) || uri.endsWith("login.jsp") || uri.endsWith("register") || uri.endsWith("register.jsp");
-        boolean resourceRequest = uri.contains("/css/") || uri.contains("/js/") || uri.contains("/img/") || uri.contains("/fonts/") || uri.contains("/vendor/");
+    boolean loggedIn = (session != null && session.getAttribute("user") != null);
+    boolean loginRequest = uri.equals(loginURI) || uri.endsWith("login.jsp") || uri.endsWith("register") || uri.endsWith("register.jsp");
+    boolean resourceRequest = uri.contains("/css/") || uri.contains("/js/") || uri.contains("/img/") || uri.contains("/fonts/") || uri.contains("/vendor/");
 
-        if (loggedIn && (loginRequest)) {
-            res.sendRedirect(req.getContextPath() + "/"); // redirect ke halaman utama jika sudah login
-        } else if (loggedIn && (uri.endsWith("register") || uri.endsWith("register.jsp"))) {
-            res.sendRedirect(req.getContextPath() + "/"); // redirect ke halaman utama jika sudah login
-        } else if (loggedIn || loginRequest || resourceRequest) {
-            chain.doFilter(request, response);
-        } else {
-            res.sendRedirect(loginURI);
-        }
+    if (loggedIn && (loginRequest)) {
+      res.sendRedirect(req.getContextPath() + "/"); // redirect ke halaman utama jika sudah login
+    } else if (loggedIn && (uri.endsWith("register") || uri.endsWith("register.jsp"))) {
+      res.sendRedirect(req.getContextPath() + "/"); // redirect ke halaman utama jika sudah login
+    } else if (loggedIn || loginRequest || resourceRequest) {
+      chain.doFilter(request, response);
+    } else {
+      res.sendRedirect(loginURI);
     }
+  }
 
-    @Override
-    public void init(FilterConfig filterConfig) {}
+  @Override
+  public void init(FilterConfig filterConfig) {}
 
-    @Override
-    public void destroy() {}
+  @Override
+  public void destroy() {}
 }
